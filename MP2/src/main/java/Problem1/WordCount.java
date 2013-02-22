41aefae222564c3785e749b39e0dca1bc97316b5
thomas-student <johnst26@illinois.edu>
2013-02-22 12:10:17 -0600

    works, added format fix

--- MP2/src/main/java/Problem1/WordCount.java
+++ MP2/src/main/java/Problem1/WordCount.java
@@ -42,17 +42,18 @@ public class WordCount {
         
  public static void main(String[] args) throws Exception {
     Configuration conf = new Configuration();
-
-     Job job = new Job(conf, "wordcount");
+    conf.set("mapred.textoutputformat.separator","-");
+    Job job = new Job(conf, "wordcount");
     
     job.setOutputKeyClass(Text.class);
     job.setOutputValueClass(IntWritable.class);
     job.setMapperClass(Map.class);
     job.setReducerClass(Reduce.class);
+    
     job.setJarByClass(WordCount.class);    
     job.setInputFormatClass(TextInputFormat.class);
     job.setOutputFormatClass(TextOutputFormat.class);
-        
+    
     FileInputFormat.addInputPath(job, new Path(args[0]));
     FileOutputFormat.setOutputPath(job, new Path(args[1]));
     
